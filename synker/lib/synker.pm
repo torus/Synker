76a7f94e2bfaf55357fe551d5ec8ac19e0fda924
Toru Hisai <toru@torus.jp>
2011-09-26 00:07:50 -1000

    loads history data on start up.

--- synker/lib/synker.pm
+++ synker/lib/synker.pm
@@ -192,10 +192,31 @@ sub store_changes {
 
     use Dancer::FileUtils 'open_file';
     my $out = open_file('>>', "hoge.xml") or die;
-    print $out "\n<!-- @{[scalar localtime]} -->\n";
     print $out $serialized;
+    print $out "\n<!-- @{[scalar localtime]} -->\n\0"; # \0 for delimiter
 
     $out->close;
+}
+
+sub load_changes {
+    use Dancer::FileUtils 'open_file';
+    my $out = eval {open_file('<', "hoge.xml")};
+
+    if (defined $out) {
+	local $/ = "\0";
+	while (my $xml = <$out>) {
+	    my $doc = XML::LibXML->load_xml (string => $xml);
+	    my ($state_id, @changes) = eval {read_updates ($doc)};
+
+	    $count = $state_id + 1;
+	    my $updates = bless {state_id => $state_id, changes => \@changes} => "synker::Updates";
+	    push @$history, $updates;
+
+	    # store_changes ($updates);
+	    apply_changes ($storage, \@changes);
+
+	}
+    }
 }
 
 post '/push' => sub {
@@ -333,4 +354,6 @@ sub toLazyXMLElement {
 }
 
 package synker;
+
+load_changes ();
 true;
