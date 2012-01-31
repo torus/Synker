5bfc8f0f1efa20e1163e761d6ca0175976dc2d58
Toru Hisai <toru@torus.jp>
2012-02-01 00:18:56 +0900

    added form

--- synker/public/tasks/tasks.js
+++ synker/public/tasks/tasks.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     console.debug("hoge!")
+    var body = $("body")
     var ul = $("<ul>")
-    $("body").append(ul)
+    body.append(ul)
+
+    var ta = $("<textarea>")
+    var form = $("<form>").append(ta).append($("<input type='submit'>"))
+    body.append(form)
+    form.submit(function(ev) {
+        var mesg = ta.val()
+        ta.val("")
+        console.debug("submit", mesg, ev)
+        send_message(mesg)
+        return false
+    })
 
     $.ajax({url: "/snapshot/",
             success: function(data, text_status, jqXHR) {
@@ -17,7 +29,29 @@ $(document).ready(function() {
                         console.debug(key, val)
                         obj[key] = val
                     }
-                    ul.append($("<li>").text("node " + obj.x + " = " + obj.y))
+                    ul.append($("<li>").text("node " + obj.state + " = " + obj.title))
                 }
             }})
+
+    // xmlmatch_test_main ()
 })
+
+function send_message(mesg) {
+    var e = E_("x", {},
+               E_("updates", {},
+                  E_("new_object", {object_id: Date.now() + "." +
+                                    Math.floor(Math.random() * 10000000)},
+                     E_("property", {key: "title"}, mesg),
+                     E_("property", {key: "state"}, "todo"),
+                     E_("property", {key: "created"}, Date.now()),
+                     E_("property", {key: "modified"}, Date.now()))))
+    var elem = e(document)
+    var xml = elem.innerHTML
+
+    var data = "update=" + encodeURI(xml)
+    console.debug(data)
+
+    $.ajax({url: "/push",
+            type: "POST",
+            data: data})
+}
