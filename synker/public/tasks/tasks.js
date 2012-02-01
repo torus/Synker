88c05235a36b6a856634cca9de31c1f137aab1a4
Toru Hisai <toru@torus.jp>
2012-02-01 23:46:33 +0900

    rewriting with xml matcher

--- synker/public/tasks/tasks.js
+++ synker/public/tasks/tasks.js
@@ -1,3 +1,6 @@
+Tasks = function() {
+}
+
 $(document).ready(function() {
     console.debug("hoge!")
     var body = $("body")
@@ -15,26 +18,63 @@ $(document).ready(function() {
         return false
     })
 
+    var tasks = new Tasks
+
     $.ajax({url: "/snapshot/",
             success: function(data, text_status, jqXHR) {
                 console.debug(data, text_status, jqXHR)
 
-                var root = data.firstChild
-                var state_id = root.getAttribute("state_id")
-                for (var child = root.firstChild; child; child = child.nextSibling) {
-                    var obj = {}
-                    for (var elem = child.firstChild; elem; elem = elem.nextSibling) {
-                        var key = elem.getAttribute("key")
-                        var val = elem.textContent
-                        console.debug(key, val)
-                        obj[key] = val
-                    }
-                    ul.append($("<li>").text("node " + obj.state + " = " + obj.title))
-                }
+                // var root = data.firstChild
+                // var state_id = root.getAttribute("state_id")
+                // for (var child = root.firstChild; child; child = child.nextSibling) {
+                //     var obj = {}
+                //     for (var elem = child.firstChild; elem; elem = elem.nextSibling) {
+                //         var key = elem.getAttribute("key")
+                //         var val = elem.textContent
+                //         console.debug(key, val)
+                //         obj[key] = val
+                //     }
+                //     ul.append($("<li>").text("node " + obj.state + " = " + obj.title))
+                //     objs[child.getAttribute("object_id")] = obj
+                // }
+
+                tasks.match_snapshot_xml(data)
+
+                console.debug(tasks)
             }})
 
     // xmlmatch_test_main ()
 })
+
+Tasks.prototype.match_snapshot_xml = function (data) {
+    with (xmlmatch) {
+        var mat =
+            M("snapshot",
+              function (e) {
+                  var state_id = e.getAttribute("state_id")
+                  console.debug("state_id", state_id)
+                  return true
+              },
+              C((function () {
+                  var obj = {prop: {}}
+                  return M("object",
+                           function (e) {
+                               var objid = e.getAttribute("object_id")
+                               obj.id = objid
+                               console.debug("object_id", objid)
+                               return true
+                           },
+                           C((function () {
+                               return M("property",
+                                        function (e) {
+                                            var key = e.getAttribute("key")
+                                            console.debug("key", key)
+                                            return true
+                                        })})()))})()))
+        var res = mat(data.firstChild)
+        console.debug("res", res)
+    }
+}
 
 function send_message(mesg) {
     var e = E_("x", {},
