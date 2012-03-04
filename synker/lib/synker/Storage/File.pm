12b006d5b4075eda9e680dcb3b0f24ef2cdc5f25
Toru Hisai <toru-github@torus.jp>
2012-03-04 15:02:36 +0900

    clean up

--- synker/lib/synker/Storage/File.pm
+++ synker/lib/synker/Storage/File.pm
@@ -39,32 +39,12 @@ sub load_changes {
                 warn "$!: $@";
             }
 
-            # open DEBUG, ">> debug.tmp" or die;
-            # print DEBUG "$doc, $xml\n";
-            # print DEBUG join (", ", @changes), "\n";
-            # print DEBUG join (", ", $state_id, $$count_ref);
-            # print DEBUG "\n=========\n";
-            # close DEBUG;
-
 	    $$count_ref = $state_id + 1;
 	    my $updates = bless {state_id => $state_id,
 				 changes => \@changes} => "synker::Updates";
 	    push @$history, $updates;
 
-            # open DEBUG, ">> debug.tmp" or die;
-            # print DEBUG "$history\n";
-            # print DEBUG "$updates\n";
-            # print DEBUG "\n=========2\n";
-            # close DEBUG;
-
-
 	    synker::apply_changes ($storage, \@changes);
-
-            # open DEBUG, ">> debug.tmp" or die;
-            # print DEBUG "@{[(%$storage)]}\n";
-            # print DEBUG "@changes\n";
-            # print DEBUG "\n=========3\n";
-            # close DEBUG;
 	}
     }
 }
